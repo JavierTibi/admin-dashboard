0d95212cd7e21e171baed4ef5ed8adf9957da170
Javier Tibi <javiert@ornsoft.com>
2020-04-09 17:19:39 -0400

    Server Js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-app = express();
-app.use(serveStatic(__dirname + "/dist"))
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+const server = require('server')
+const { get, post } = server.router
+const { render, redirect } = server.reply
+
+server(
+  get('/', () => render('index.pug')),
+  post('/', ctx => {
+    console.log(ctx.data)
+    return redirect('/')
+  })
+)
