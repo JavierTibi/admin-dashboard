01563577fbc7a2112292ad8555ba6d1f5855e822
Javier Tibi <javiert@ornsoft.com>
2020-04-11 12:26:17 -0400

    Add server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
+// Include it and extract some methods for convenience
 const server = require('server')
-const { get, post } = server.router
-const { render, redirect } = server.reply
+const { get } = server.router
 
-server(
-  get('/', () => render('index.pug')),
-  post('/', ctx => {
-    console.log(ctx.data)
-    return redirect('/')
-  })
-)
+// Launch server with options and a couple of routes
+server({ port: 80 }, [
+  get('/')
+])
